be262e6da923b3484ca40db038dc481793af78ee
PogoGuru <131234504+Involvee@users.noreply.github.com>
2025-11-28 13:33:30 +0500

    modify base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  base: '/Portfolio-V2/',
+  base: '/Portfolio/',
   plugins: [
     react(),
     ViteImageOptimizer({
